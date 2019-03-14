fcb2391adbc753d43f47cfaa004f3f68571719a5
crazybill <1007950441@11.com>
2019-03-14 16:43:36 +0800

    Default Changelist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from urllib import request
 import time
+import os
 
 while True:
     cmd = request.urlopen("http://api.itmojun.com/pc/cmd/get?id=djb").read()
@@ -7,6 +8,19 @@ while True:
 
     if cmd !="":
         print(cmd)
+
+        if"关机"in cmd:
+            # os.system("shutdown -s -t 0")
+            pass
+        elif"重启"in cmd:
+            # os.system("shutdown -r -t 0")
+            pass
+        elif"网站" in cmd:
+            os.system("explorer https://www.baidu.com")
+        elif"播放" in cmd:
+            os.system('c.mp3')
+
+
         time.sleep(3)
     else:
         time.sleep(1)
